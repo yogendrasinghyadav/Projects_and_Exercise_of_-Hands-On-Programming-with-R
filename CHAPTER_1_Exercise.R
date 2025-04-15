96bb272f4dde1c9bb96170a1985a90482c4c3dc3
Yogendra Singh Yadav <115386082+yogendrasinghyadav@users.noreply.github.com>
2025-04-15 20:22:07 +0530

    Add files via upload

---
+++ CHAPTER_1_Exercise.R
@@ -0,0 +1,14 @@
+#CHAPTER 1 > Exercise
+# That’s the basic interface for executing R code in RStudio. Think you have it? If so, try
+# doing these simple tasks. If you execute everything correctly, you should end up with
+# the same number that you started with:
+# 1. Choose any number and add 2 to it.
+# 2. Multiply the result by 3.
+# 3. Subtract 6 from the answer.
+# 4. Divide what you get by 3.
+x <- 5
+x <- x+2
+x <- x*3
+x <- x-6
+x <- x/3
+print(x)
